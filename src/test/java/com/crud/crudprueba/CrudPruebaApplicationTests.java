6660b69c22d5fa98274a67b613644d517a48b56d
jucaji <jcjvirtual@gmail.com>
2023-01-27 19:04:29 -0500

    first commit

---
+++ src/test/java/com/crud/crudprueba/CrudPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.crudprueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
