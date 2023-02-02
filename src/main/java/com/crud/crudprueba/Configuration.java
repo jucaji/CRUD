07e381febe0f705516ac53040cfbefa5bca3ec43
jucaji <jcjvirtual@gmail.com>
2023-02-02 12:26:56 -0500

    feat : "refactorizando"

--- src/main/java/com/crud/crudprueba/Configuration.java
+++ src/main/java/com/crud/crudprueba/Configuration.java
@@ -2,9 +2,18 @@ package com.crud.crudprueba;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+
+/**
+ * Clase de configuracion para ModelMapper
+ * @author Julian Jimenez
+ */
 @org.springframework.context.annotation.Configuration
 public class Configuration {
 
+    /**
+     * Metodo para crear un bean de ModelMapper
+     * @return ModelMapper
+     */
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
