20c1415a14367b520b9966d816e03026b34d9393
jucaji <jcjvirtual@gmail.com>
2023-02-02 12:04:17 -0500

    feat : "refactorizando"

--- src/main/java/com/crud/crudprueba/data/crud/ITripCrudRepository.java
+++
@@ -1,7 +0,0 @@
-package com.crud.crudprueba.data.crud;
-
-import com.crud.crudprueba.data.entity.Trip;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ITripCrudRepository extends JpaRepository<Trip, Long> {
-}
